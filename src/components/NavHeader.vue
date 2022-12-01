8840f93f0bdba773c1831782d78fa96f98b3b3b3
LUO <2693631615@qq.com>
2022-12-01 17:02:09 +0800

    项目目录搭建

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav-header">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav-header',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
